e2415657648e8338cfe9b92d1a51c87637ac42d4
Jonathan Talavera <jtalavera@g.ucla.edu>
2019-01-13 21:33:16 -0800

    first files

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+// Import the database connection
+var connection = require("../config/connection.js");
+
+function selectAll(){
+
+}
+
+function insertOne(){
+
+}
+
+function updateOne(){
+    
+}
+
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('McDouble');
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
